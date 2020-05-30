f21bb90359183cc906481aeb9dcdf19676b69a98
Pierre Genthon <pierre.genthon@wildcodeschool.fr>
2020-05-30 11:29:19 +0200

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+// parse requests of content-type: application/json
+app.use(express.json());
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "It works" });
+});
+
+require("./routes/customer.routes.js")(app);
+
+// set port, listen for requests
+app.listen(3000, () => {
+  console.log("Server is running on port 3000.");
+});
