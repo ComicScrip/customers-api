a6f3d53d24d9a825c40817e1a73ab8cc884a9dca
Pierre Genthon <pierre.genthon@wildcodeschool.fr>
2020-05-30 13:43:39 +0200

    port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // parse requests of content-type: application/json
 app.use(express.json());
@@ -13,6 +14,6 @@ app.get("/", (req, res) => {
 require("./routes/customer.routes.js")(app);
 
 // set port, listen for requests
-app.listen(3000, () => {
-  console.log("Server is running on port 3000.");
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
 });
